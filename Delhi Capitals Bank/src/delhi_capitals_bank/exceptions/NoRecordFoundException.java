b87346d0e4b870d42270ced24bb7b700f69c5ef1
Manjeet <godaramanjeet293@gmail.com>
2023-04-02 17:32:50 +0530

    Project done

--- Delhi Capitals Bank/src/delhi_capitals_bank/exceptions/NoRecordFoundException.java
+++
@@ -1,7 +0,0 @@
-package delhi_capitals_bank.exceptions;
-
-public class NoRecordFoundException extends Exception {
-	 public NoRecordFoundException(String msg) {
-		   super(msg);
-	   }
-}
--- Delhi Capitals Bank/src/delhi_capitals_bank/exceptions/SomethingWentWrongException.java
+++
@@ -1,7 +0,0 @@
-package delhi_capitals_bank.exceptions;
-
-public class SomethingWentWrongException extends Exception {
-   public SomethingWentWrongException(String msg) {
-	   super(msg);
-   }
-}
